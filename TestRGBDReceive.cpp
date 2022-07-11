ce2d0f730e7e764919de9c75b58e88a8f1079f26
zhanghaodan <pkuzhd@pku.edu.cn>
2022-07-11 16:44:29 +0800

    RGBD test

--- TestRGBDReceive.cpp
+++ TestRGBDReceive.cpp
@@ -15,13 +15,22 @@ using namespace std;
 void save(string path, int idx, RGBDData *data) {
     for (int i = 0; i < data->n; ++i) {
         cv::Mat img(data->h, data->w, CV_8UC3);
-        memcpy(img.data, data->getDepth(i), data->w * data->h * 3);
-        cv::imwrite(path + to_string(idx) + "-" + to_string(i) + ".jpg", img);
+        memcpy(img.data, data->getImage(i), data->w * data->h * 3);
+        cv::imwrite(path + to_string(idx + 1) + "-" + to_string(i + 1) + ".jpg", img);
     }
 }
 
 int main() {
-    string test_path = "/test_dir/";
+    string test_path = "./test_dir/";
+    for (int j = 0; j < 5; ++j) {
+        for (int i = 0; i < 5; ++i) {
+            cv::Mat img = cv::imread(string("./test_data/" + to_string((j + i) % 5 + 1) + ".jpg"));
+            cv::Mat img_gt(2160, 3840, CV_8UC3);
+            memcpy(img_gt.data, img.data, 2160 * 3840 * 3);
+            cv::imwrite(test_path + to_string(j + 1) + "-" + to_string(i + 1) + "-gt.jpg", img_gt);
+        }
+    }
+
     RGBDReceiver receiver;
     receiver.open("pipe filename");
 
