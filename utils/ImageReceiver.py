d7920fea3e6dac9150e27da40ac09b9e88627fc8
Wang Haoyang <harry_18@bupt.edu.cn>
2022-07-14 21:25:25 +0800

    update file

--- utils/ImageReceiver.py
+++ utils/ImageReceiver.py
@@ -26,13 +26,32 @@ class ImageReceiver():
         buf = os.read(self.rf, 1)
         image_data.N = int.from_bytes(buf, byteorder='little', signed=False)
         print(image_data.N)
+
+
+
         for i in range(image_data.N):
             buf = os.read(self.rf, 4)
             w = int.from_bytes(buf, byteorder='little', signed=False)
             buf = os.read(self.rf, 4)
             h = int.from_bytes(buf, byteorder='little', signed=False)
             print(w, h)
-            buf = os.read(self.rf, w * h * 3)
+
+            buf = b''
+            read_len = 0
+            i = 0
+            while read_len + 65536 * 16 < w * h * 3:
+                buf += os.read(self.rf, 65536 * 16)
+                read_len = np.frombuffer(buf, np.uint8).size
+                i += 1
+
+            print(w * h * 3, read_len, w * h * 3 - read_len)
+            while read_len + 1024 < w * h * 3:
+                buf += os.read(self.rf, 1024)
+                read_len = np.frombuffer(buf, np.uint8).size
+
+            print(w * h * 3, read_len, w * h * 3 - read_len)
+            buf += os.read(self.rf, w * h * 3 - read_len)
+
             image_data.imgs.append(np.frombuffer(buf, np.uint8).reshape((h, w, 3)))
 
         return image_data
