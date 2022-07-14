fb27d5b0c7343999be61ec402043e707aaa33f17
Wang Haoyang <harry_18@bupt.edu.cn>
2022-07-14 19:31:57 +0800

    update files

--- utils/ImageReceiver.py
+++ utils/ImageReceiver.py
@@ -1,5 +1,6 @@
 import cv2
-
+import os
+import numpy as np
 
 class ImageData():
     def __init__(self, N, imgs):
@@ -9,13 +10,33 @@ class ImageData():
 
 class ImageReceiver():
     def __init__(self):
-        pass
+        self.rf = 0
 
     def open(self, filename):
-        pass
+        if not os.path.exists(filename):
+            os.mkfifo(filename)
+        self.rf = os.open(filename, os.O_RDONLY)
+        print("os.open finished")
 
     def close(self):
-        pass
+        os.close(self.rf)
 
     def getData(self) -> ImageData:
-        pass
+        image_data = ImageData(0, [])
+        buf = os.read(self.rf, 1)
+        image_data.N = int.from_bytes(buf, byteorder='little', signed=False)
+        print(image_data.N)
+        for i in range(image_data.N):
+            buf = os.read(self.rf, 4)
+            w = int.from_bytes(buf, byteorder='little', signed=False)
+            buf = os.read(self.rf, 4)
+            h = int.from_bytes(buf, byteorder='little', signed=False)
+            print(w, h)
+            buf = os.read(self.rf, w * h * 3)
+            image_data.imgs.append(np.frombuffer(buf, np.uint8).reshape((h, w, 3)))
+
+        return image_data
+
+
+
+
