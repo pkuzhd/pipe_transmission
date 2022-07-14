f4edfa59fd1d0f56f5814754e90c01b81cfc85e2
zhanghaodan <pkuzhd@pku.edu.cn>
2022-07-14 12:55:27 +0800

    add TestImage

--- TestImageReceive.py
+++ TestImageReceive.py
@@ -1,2 +1,14 @@
 import cv2
 
+from utils.ImageReceiver import ImageData, ImageReceiver
+
+recv = ImageReceiver()
+
+recv.open("./pipe_dir/pipe1")
+
+for i in range(5):
+    data = recv.getData()
+    for j in range(5):
+        cv2.imwrite(f"./test_dir/{j+1}-{i+1}.jpg", data.imgs[j])
+
+recv.close()
