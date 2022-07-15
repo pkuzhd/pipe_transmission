1c8824c85d886c243ff1bbb10d01b0980dfa6b0e
zhanghaodan <pkuzhd@pku.edu.cn>
2022-07-15 09:34:35 +0800

    fixbug

--- TestImageReceive.py
+++ TestImageReceive.py
@@ -9,6 +9,6 @@ recv.open("./pipe_dir/pipe1")
 for i in range(5):
     data = recv.getData()
     for j in range(5):
-        cv2.imwrite(f"./test_dir/{j+1}-{i+1}.jpg", data.imgs[j])
+        cv2.imwrite(f"./test_dir/{i+1}-{j+1}.jpg", data.imgs[j])
 
 recv.close()
