ef3714ffe71b42518aaf59ad2ed8fa3549c3f560
Harrywang18 <1055679009@qq.com>
2022-07-21 00:59:41 +0800

    update files

--- utils/ImageReceiver.py
+++ utils/ImageReceiver.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import numpy as np
+import fcntl
 
 class ImageData():
     def __init__(self, N, imgs):
@@ -17,6 +18,7 @@ class ImageReceiver():
             os.mkfifo(filename)
         self.rf = os.open(filename, os.O_RDONLY)
         print("os.open finished")
+        print("pipe size", fcntl.fcntl(self.rf, 1032))
 
     def close(self):
         os.close(self.rf)
@@ -25,7 +27,7 @@ class ImageReceiver():
         image_data = ImageData(0, [])
         buf = os.read(self.rf, 1)
         image_data.N = int.from_bytes(buf, byteorder='little', signed=False)
-        print(image_data.N)
+        #print(image_data.N)
 
 
 
@@ -34,7 +36,7 @@ class ImageReceiver():
             w = int.from_bytes(buf, byteorder='little', signed=False)
             buf = os.read(self.rf, 4)
             h = int.from_bytes(buf, byteorder='little', signed=False)
-            print(w, h)
+            #print(w, h)
 
             buf = b''
             read_len = 0
@@ -42,14 +44,8 @@ class ImageReceiver():
             while read_len + 65536 * 16 < w * h * 3:
                 buf += os.read(self.rf, 65536 * 16)
                 read_len = np.frombuffer(buf, np.uint8).size
-                i += 1
 
-            print(w * h * 3, read_len, w * h * 3 - read_len)
-            while read_len + 1024 < w * h * 3:
-                buf += os.read(self.rf, 1024)
-                read_len = np.frombuffer(buf, np.uint8).size
-
-            print(w * h * 3, read_len, w * h * 3 - read_len)
+            #print(w * h * 3, read_len, w * h * 3 - read_len)
             buf += os.read(self.rf, w * h * 3 - read_len)
 
             image_data.imgs.append(np.frombuffer(buf, np.uint8).reshape((h, w, 3)))
