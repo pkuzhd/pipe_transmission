c4c22989f42cadce6d804c269dd50ee317db8d28
Harrywang18 <1055679009@qq.com>
2022-07-21 14:50:56 +0800

    add time calculation

--- utils/ImageReceiver.py
+++ utils/ImageReceiver.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import numpy as np
 import fcntl
+import datetime
 
 class ImageData():
     def __init__(self, N, imgs):
@@ -41,20 +42,48 @@ class ImageReceiver():
             buf = b''
             read_len = 0
             i = 0
+            
+            start_time = datetime.datetime.now()
+            
+            
+            
             while read_len + 65536 * 16 < w * h * 3:
                 buf += os.read(self.rf, 65536 * 16)
                 read_len = np.frombuffer(buf, np.uint8).size
+                
+            end_time = datetime.datetime.now()
+            time_cost = ((end_time - start_time).seconds * 1000 + (end_time - start_time).microseconds / 1000)
+            
+            print("65536 * 16:", time_cost)
+            
+            
+            start_time = datetime.datetime.now()
             
             while read_len + 65536 < w * h * 3:
             #print(w * h * 3, read_len, w * h * 3 - read_len)
                 buf += os.read(self.rf, 65536)
                 read_len = np.frombuffer(buf, np.uint8).size
-        
+                
+            end_time = datetime.datetime.now()
+            time_cost = ((end_time - start_time).seconds * 1000 + (end_time - start_time).microseconds / 1000)
+            
+            print("65536:", time_cost)
+            
+            start_time = datetime.datetime.now()
+            
             buf += os.read(self.rf, w * h * 3 - read_len)
+            
+            end_time = datetime.datetime.now()
+            time_cost = ((end_time - start_time).seconds * 1000 + (end_time - start_time).microseconds / 1000)
+            
+            print("remain:", time_cost)
+            
+            
             image_data.imgs.append(np.frombuffer(buf, np.uint8).reshape((h, w, 3)))
 
         return image_data
 
+    
 
 
 
